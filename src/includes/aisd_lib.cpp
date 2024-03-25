2a2e3906cb66b747829a63411da822963e28e419
igorciz777 <igorciz777@gmail.com>
2024-03-25 15:54:16 +0100

    Zmiana na polski, niepelna implementacja grafu

--- src/includes/aisd_lib.cpp
+++ src/includes/aisd_lib.cpp
@@ -8,3 +8,42 @@
  * 
 */
 #include "aisd_lib.h"
+
+Wierzcholek::Wierzcholek(int wartosc){
+    this->wartosc = wartosc;
+}
+
+Krawedz::Krawedz(Wierzcholek *wierzcholek1, Wierzcholek *wierzcholek2, int waga){
+    this->wierzcholek1 = wierzcholek1;
+    this->wierzcholek2 = wierzcholek2;
+    this->waga = waga;
+}
+
+Graf::Graf(){
+    this->liczbaWierzcholkow = 0;
+    this->liczbaKrawedzi = 0;
+}
+Graf::Graf(int liczbaWierzcholkow, int liczbaKrawedzi){
+    this->liczbaWierzcholkow = liczbaWierzcholkow;
+    this->liczbaKrawedzi = liczbaKrawedzi;
+    this->wierzcholki = std::vector<Wierzcholek>(liczbaWierzcholkow);
+    this->krawedzie = std::vector<Krawedz>(liczbaKrawedzi);
+}
+void Graf::dodajWierzcholek(int wartosc){
+    this->wierzcholki.push_back(Wierzcholek(wartosc));
+}
+void Graf::dodajWiezcholek(Wierzcholek wierzcholek){
+    this->wierzcholki.push_back(wierzcholek);
+}
+void Graf::dodajKrawedz(Wierzcholek *wierzcholek1, Wierzcholek *wierzcholek2, int waga){
+    this->krawedzie.push_back(Krawedz(wierzcholek1, wierzcholek2, waga));
+}
+void Graf::dodajKrawedz(Krawedz krawedz){
+    this->krawedzie.push_back(krawedz);
+}
+void Graf::getMacierzSasiedztwa(){
+    this->macierzSasiedztwa = std::vector<std::vector<int>>(this->liczbaWierzcholkow, std::vector<int>(this->liczbaWierzcholkow, 0));
+    for(int i = 0; i < this->liczbaKrawedzi; i++){
+        this->macierzSasiedztwa[this->krawedzie[i].wierzcholek1->wartosc - 'A'][this->krawedzie[i].wierzcholek2->wartosc - 'A'] = this->krawedzie[i].waga;
+    }
+}
--- src/includes/aisd_lib.h
+++ src/includes/aisd_lib.h
@@ -3,56 +3,40 @@
  * @brief Biblioteka do obsługi struktur danych i algorytmów
 */
 
-/**
- * @brief Struktura reprezentująca wierzchołek
- * 
- * Struktura reprezentująca wierzchołek w grafie
- * @param data - wartość wierzchołka
-*/
-struct Node{
-    char data;
+#include <vector>
+
+struct Wierzcholek{
+    int wartosc;
+    Wierzcholek(int wartosc);
 };
 
-/**
- * @brief Struktura reprezentująca krawędź
- * 
- * Struktura reprezentująca krawędź w grafie
- * @param src - wskaźnik na wierzchołek źródłowy
- * @param dest - wskaźnik na wierzchołek docelowy
-*/
-struct Edge{
-    struct Node *src;
-    struct Node *dest;
+struct Krawedz{
+    struct Wierzcholek *wierzcholek1;
+    struct Wierzcholek *wierzcholek2;
+    int waga;
+    Krawedz(struct Wierzcholek *wierzcholek1, struct Wierzcholek *wierzcholek2, int waga);
 };
 
-/**
- * @brief Struktura reprezentująca krawędź w sieci przepływowej
- * 
- * Struktura reprezentująca krawędź w sieci przepływowej
- * @param capacity - pojemność krawędzi
- * @param flow - przepływ krawędzi
-*/
-struct Arc : public Edge{
-    int capacity;
-    int flow;
+struct Graf{
+    int liczbaWierzcholkow;
+    int liczbaKrawedzi;
+    std::vector<std::vector<int>> macierzSasiedztwa;
+    std::vector<struct Wierzcholek> wierzcholki;
+    std::vector<struct Krawedz> krawedzie;
+    Graf();
+    Graf(int liczbaWierzcholkow, int liczbaKrawedzi);
+    void dodajWierzcholek(int wartosc);
+    void dodajWiezcholek(struct Wierzcholek wierzcholek);
+    void dodajKrawedz(struct Wierzcholek *wierzcholek1, struct Wierzcholek *wierzcholek2, int waga);
+    void dodajKrawedz(struct Krawedz krawedz);
+    void getMacierzSasiedztwa();
 };
 
-/**
- * @brief Struktura reprezentująca sieć przepływową
- * 
- * Struktura reprezentująca sieć przepływową
- * @param V - liczba wierzchołków
- * @param E - liczba krawędzi
- * @param arc - tablica krawędzi
- * @param node - tablica wierzchołków
- * @param start - wskaźnik na wierzchołek źródłowy
- * @param target - wskaźnik na wierzchołek docelowy
-*/
-struct FlowNetwork{
-    int V, E;
-    struct Arc *arc;
-    struct Node *node;
-    struct Node *start, *target;
-    int **adjMatrix;
+struct SiecPrzeplywowa : Graf{
+    struct Wierzcholek *zrodlo; // S
+    struct Wierzcholek *ujscie; // T
+    SiecPrzeplywowa();
 };
+
+
 
