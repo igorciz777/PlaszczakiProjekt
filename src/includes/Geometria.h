18fa3dd25cfa3c65ea1b79990dda922434cd9065
igorciz777 <igorciz777@gmail.com>
2024-04-01 16:11:39 +0200

    Przeniesienie Jarvis_march.cpp do Geometria.h/cpp

--- src/includes/Geometria.h
+++ src/includes/Geometria.h
@@ -3,3 +3,39 @@
  * @brief Plik nagłówkowy dla implementacji struktur i algorytmów geometrycznych
  * 
 */
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct Punkt2{
+    double x;
+    double y;
+};
+
+struct Punkt3{
+    double x;
+    double y;
+    double z;
+};
+
+struct Macierz3x3{
+    double a11, a12, a13;
+    double a21, a22, a23;
+    double a31, a32, a33;
+    Macierz3x3();
+    Macierz3x3(double a11, double a12, double a13, double a21, double a22, double a23, double a31, double a32, double a33);
+    void setKolumna1(Punkt3 p);
+    void setKolumna2(Punkt3 p);
+    void setKolumna3(Punkt3 p);
+    double wyznacznik();
+};
+
+class OtoczkaJarvis{
+    public:
+        OtoczkaJarvis();
+        void stworzOtoczke(Punkt2 punkty[], int n);
+    private:
+        int orientacja(Punkt2 a, Punkt2 b, Punkt2 c);
+        std::vector<int> nastPunkt;
+        std::vector<Punkt2> punkty;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
-#include "includes/aisd_lib.h"
+#include "includes/Geometria.cpp"
+#include <iostream>
+
+int main(){
+    Punkt2 punkty[] = {{0, 3}, {2, 2}, {1,1}, {2, 1}, {3, 0}, {0, 0}, {3, 3}};
+    std::cout << "Punkty: ";
+    int n = sizeof(punkty) / sizeof(punkty[0]);
+    OtoczkaJarvis otoczka = OtoczkaJarvis();
+    otoczka.stworzOtoczke(punkty, n);
+    return 0;
+}
