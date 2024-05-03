4363032b548316ea1f6cd228edb5c81dbf1d84a0
igorciz777 <igorciz777@gmail.com>
2024-05-03 16:51:43 +0200

    Algorytm Aho-Corasick

---
+++ src/includes/AhoCorasick.cpp
@@ -0,0 +1,152 @@
+#include "AhoCorasick.h"
+
+Wezel::Wezel()
+{
+    sufiks = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        nast[i] = 0;
+    }
+}
+
+int AhoCorasick::nast_id = 0;
+
+AhoCorasick::AhoCorasick(std::vector<std::string> &p)
+{
+    trie = new Wezel();
+    for (int i = 0; i < p.size(); i++)
+    {
+        wstaw(trie, p[i], i);
+    }
+    std::queue<Wezel*> q;
+    for (int i = 0; i < 256; i++)
+    {
+        if (trie->nast[i])
+        {
+            trie->nast[i]->sufiks = trie;
+            q.push(trie->nast[i]);
+        }
+    }
+    while (!q.empty())
+    {
+        Wezel* r = q.front();
+        q.pop();
+        for (int i = 0; i < 256; i++)
+        {
+            if (r->nast[i])
+            {
+                Wezel* x = r->sufiks;
+                while (x != trie && !x->nast[i])
+                {
+                    x = x->sufiks;
+                }
+                if (x->nast[i])
+                {
+                    x = x->nast[i];
+                }
+                r->nast[i]->sufiks = x;
+                for (int j = 0; j < x->wzorce.size(); j++)
+                {
+                    r->nast[i]->wzorce.push_back(x->wzorce[j]);
+                }
+                q.push(r->nast[i]);
+            }
+        }
+    }
+}
+
+AhoCorasick::AhoCorasick(std::string p){
+    trie = new Wezel();
+    wstaw(trie, p, 0);
+    std::queue<Wezel*> q;
+    for (int i = 0; i < 256; i++)
+    {
+        if (trie->nast[i])
+        {
+            trie->nast[i]->sufiks = trie;
+            q.push(trie->nast[i]);
+        }
+    }
+    while (!q.empty())
+    {
+        Wezel* r = q.front();
+        q.pop();
+        for (int i = 0; i < 256; i++)
+        {
+            if (r->nast[i])
+            {
+                Wezel* x = r->sufiks;
+                while (x != trie && !x->nast[i])
+                {
+                    x = x->sufiks;
+                }
+                if (x->nast[i])
+                {
+                    x = x->nast[i];
+                }
+                r->nast[i]->sufiks = x;
+                for (int j = 0; j < x->wzorce.size(); j++)
+                {
+                    r->nast[i]->wzorce.push_back(x->wzorce[j]);
+                }
+                q.push(r->nast[i]);
+            }
+        }
+    }
+}
+
+void AhoCorasick::wstaw(Wezel* x, std::string s, int id)
+{
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (!x->nast[s[i]])
+        {
+            x->nast[s[i]] = new Wezel();
+        }
+        x = x->nast[s[i]];
+    }
+    x->wzorce.push_back(id);
+}
+
+Wezel* AhoCorasick::sufiks(Wezel* x, char c)
+{
+    while (x != trie && !x->nast[c])
+    {
+        x = x->sufiks;
+    }
+    if (x->nast[c])
+    {
+        x = x->nast[c];
+    }
+    return x;
+}
+
+void AhoCorasick::szukaj(std::string s)
+{
+    Wezel* x = trie;
+    for (int i = 0; i < s.size(); i++)
+    {
+        x = sufiks(x, s[i]);
+        for (int j = 0; j < x->wzorce.size(); j++)
+        {
+            std::cout << "Znaleziono wzorzec " << x->wzorce[j] << " na pozycji " << i << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    //test dzialania algorytmu
+
+    //algorytm opiera sie na sufiksach,
+    //podane indeksy sa ostatnim indeksem wzorca w tekscie
+
+    //TODO: zamiana tekstu wedlug wzorca
+    //TODO: sprawdzic obsluge unicode (polskie znaki, etc.)
+
+    std::string tekst = "polibolibolipolibolipolipoliboli";
+    std::vector<std::string> wzorce = {"poli", "boli"};
+    AhoCorasick ac(wzorce);
+    ac.szukaj(tekst);
+    return 0;
+}
---
+++ src/includes/AhoCorasick.h
@@ -0,0 +1,90 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+
+/**
+ * @brief Struktura reprezentująca węzeł drzewa Aho-Corasicka
+ * 
+ * Struktura przechowuje wskaźniki na następne węzły, wskaźnik na węzeł sufiksowy, wektory wzorców i znaków
+ * 
+ * @param sufiks Wskaźnik na węzeł sufiksowy
+ * @param nast Tablica wskaźników na następne węzły
+ * @param wzorce Wektor wzorców
+ * @param id Identyfikator węzła (dla debugowania)
+*/
+struct Wezel{
+    Wezel* sufiks;
+    Wezel* nast[256];
+    std::vector<int> wzorce;
+    int id;
+    /**
+     * @brief Konstruktor struktury Wezel
+     * 
+     * Tworzy nowy węzeł drzewa Aho-Corasicka
+    */
+    Wezel();
+};
+
+/**
+ * @brief Klasa reprezentująca algorytm Aho-Corasicka
+ * 
+ * Klasa przechowuje wskaźnik na korzeń drzewa Aho-Corasicka
+ * 
+ * @param trie Wskaźnik na korzeń drzewa Aho-Corasicka
+ * @param nast_id Identyfikator następnego węzła
+ * 
+*/
+class AhoCorasick{
+    private:
+        static int nast_id;
+        Wezel* trie;
+        /**
+         * @brief Metoda tworząca nowy węzeł
+         * 
+         * Tworzy nowy węzeł drzewa Aho-Corasicka
+         * 
+         * @return Wskaźnik na nowy węzeł
+        */
+        Wezel* sufiks(Wezel* x, char c);
+        /**
+         * @brief Metoda wstawiająca wzorzec do drzewa
+         * 
+         * Wstawia wzorzec do drzewa Aho-Corasicka
+         * 
+         * @param x Wskaźnik na węzeł
+         * @param s Wzorzec
+         * @param id Identyfikator węzła (dla debugowania)
+        */
+        void wstaw(Wezel* x, std::string s, int id);
+    public:
+        /**
+         * @brief Konstruktor klasy AhoCorasick
+         * 
+         * Tworzy obiekt klasy AhoCorasick na podstawie wektora wzorców.
+         * 
+         * @param p Wektor wzorców
+         */
+        AhoCorasick(std::vector<std::string> &p);
+        
+        /**
+         * @brief Konstruktor klasy AhoCorasick
+         * 
+         * Tworzy obiekt klasy AhoCorasick na podstawie pojedynczego wzorca.
+         * 
+         * @param p Wzorzec
+         */
+        AhoCorasick(std::string p);
+        
+        /**
+         * @brief Metoda szukająca wzorców w tekście
+         * 
+         * Szuka wszystkich wzorców z drzewa w podanym tekście.
+         * 
+         * @param s Tekst
+         */
+        void szukaj(std::string s);
+};
