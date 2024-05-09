1c38402a4d31e032fb7d48081f36381fd1165127
SzymonN <szymonnajdzion@gmail.com>
2024-05-09 18:05:21 +0200

    dobor zepsuty plik

---
+++ src/includes/dobor_tragarzy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Graf.cpp"
+
+using namespace std;
+
+
+
+int main(){
+    int iloscTragarzy;
+
+    cout << "Podaj ilosc tragazy: "; cin >> iloscTragarzy; cout << endl;
+    int tablicaTragarzy[iloscTragarzy];
+
+    cout << "Nazwij ich (liczba calkowita): " << endl;
+    for(int i = 0; i < iloscTragarzy; i++){
+        cin >> tablicaTragarzy[i];
+    }
+
+    Graf G = Graf();
+    for(int i = 0; i < iloscTragarzy; i++){
+        G.dodajWierzcholek(tablicaTragarzy[i]);
+    }
+
+
+
+
+
+    return 0;
+}
