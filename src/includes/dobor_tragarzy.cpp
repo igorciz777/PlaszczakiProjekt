0e55af6a2e30d3ac9129f9d1c4f690e355547ad5
SzymonN <szymonnajdzion@gmail.com>
2024-05-12 19:13:18 +0200

    prace nad tragarzami, dobor tragarzy

--- src/includes/dobor_tragarzy.cpp
+++ src/includes/dobor_tragarzy.cpp
@@ -7,35 +7,12 @@ using namespace std;
 
 
 int main(){
-    // int iloscTragarzy;
-
-    // cout << "Podaj ilosc tragazy: "; cin >> iloscTragarzy; cout << endl;
-    // std::vector<int> tablicaTragarzy(iloscTragarzy);
-    // SiecPrzeplywowa siec = SiecPrzeplywowa(iloscTragarzy);
-
-
-    // for(int i = 0; i < iloscTragarzy; i++){
-    //     siec.dodajWierzcholek(i);        
-    // }
-
-    // cout << "Polacz tragarzy wspopracujacych ze soba, ile par chcesz utworzyc?" << endl;
-    // int l, x, y, przepustowosc;
-    // cin >> l;
-    // for(int i = 0; i < l; i++){
-    //     cin >> x >> y >> przepustowosc;
-    //     siec.dodajKrawedz(x, y, przepustowosc);
-    // }
     
-    // cout << "Podaj fabryke i zrodlo:";
-    // int m, n;
-    // cin >> m, n;
-    // cout << siec.maxPrzeplyw(m, n);
-    int iloscTragarzy, recePrzod, receTyl;
+    int recePrzod, receTyl;
     vector<int> tragarzePrzod, tragarzeTyl, tragarzePrzodTyl; //ostatni gdyby trzeba bylo miec ich razemm i po kolei
-    cout << "Podaj ilu tragarzy bedzie budowac plot: ";
-    cin >> iloscTragarzy;
+    
 
-    cout << "Ilu z nich ma rece z przodu? ";
+    cout << "Podaj liczbe tragarzy z rekoma z przodu: ";
     cin >> recePrzod;
     // if(iloscTragarzy <= recePrzod){//TODO obsluga bledow!!
     //     cout << "zbyt";
@@ -43,7 +20,7 @@ int main(){
     for(int i = 0; i < recePrzod; i++){
         tragarzePrzod.push_back(i); //dodajemy tragarzy z rekoma w przod do vectora
     }
-    cout << "Ilu z nich ma rece z tylu? ";
+    cout << "Podaj liczbe tragarzy z rekoma z tylu: ";
     cin >> receTyl;
     for(int i = 0; i < receTyl; i++){
         tragarzeTyl.push_back(i + recePrzod); // Dodajemy i + recePrzod, aby indeksy były kontynuacją
@@ -69,30 +46,6 @@ int main(){
         cout << i << " ";
     }
     cout << endl;
-
-    
-    
-    
-
-    // cout << "tragarzePrzod:" << endl;
-    // for (int i = 0; i < recePrzod; i++){
-    //     cout << tragarzePrzod[i];
-    // } 
-    // cout << endl;
-    // cout << "tragarzeTyl:" << endl;
-    // for (int i = 0; i < receTyl; i++){
-    //     cout << tragarzeTyl[i];
-    // }
-    // cout << endl;
-    // cout << "tragarzePrzodTyl:" << endl;
-    // for (int i = 0; i < iloscTragarzy; i++){
-    //     cout << tragarzePrzodTyl[i];
-    // }
-    // cout << endl;
-
-
-
-    //SiecPrzeplywowa siec = SiecPrzeplywowa(iloscTragarzy);
 
    return 0;
 }
