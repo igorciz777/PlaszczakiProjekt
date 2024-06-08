8cf7d0d7d80044b23d688498044cd8ca359772f3
igorciz777 <igorciz777@gmail.com>
2024-06-08 16:04:02 +0200

    docs update

---
+++ docs/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dokumentacja_20techniczna_20projektu_20plaszczakiprojekt_0',['Dokumentacja techniczna projektu PlaszczakiProjekt',['../index.html',1,'']]]
+];
---
+++ docs/search/pages_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['plaszczakiprojekt_0',['Dokumentacja techniczna projektu PlaszczakiProjekt',['../index.html',1,'']]],
+  ['projektu_20plaszczakiprojekt_1',['Dokumentacja techniczna projektu PlaszczakiProjekt',['../index.html',1,'']]]
+];
